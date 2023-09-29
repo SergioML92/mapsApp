44b2db054e3817d4d5d2be12afab29311fd70fa5
sergio.mohedano <s.mohedano92@gmail.com>
2023-09-29 10:42:01 +0200

    Fin Sección 22

--- src/app/alone/components/side-menu/side-menu.component.ts
+++ src/app/alone/components/side-menu/side-menu.component.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 interface MenuItem {
   name: string;
@@ -6,9 +8,11 @@ interface MenuItem {
 }
 
 @Component({
-  selector: 'maps-side-menu',
+  selector: 'side-menu',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.css']
+  styleUrls: ['./side-menu.component.css'],
 })
 export class SideMenuComponent {
   public menuItems: MenuItem[] = [
@@ -16,5 +20,6 @@ export class SideMenuComponent {
     { name: 'ZoomRange', route: '/maps/zoom-range' },
     { name: 'Markers', route: '/maps/markers' },
     { name: 'Houses', route: '/maps/properties' },
+    { name: 'Counter', route: '/alone' },
   ]
 }
